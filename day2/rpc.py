df21330badc6fbde1ffaee364a3ea9c1170a31b9
Abdullah <ahamad@noblhealth.com>
2022-12-23 12:21:45 -0600

    first attempt for day2 puzzle

--- day2/rpc.py
+++ day2/rpc.py
@@ -1,5 +1,47 @@
+"""
+
+-----------------
+A - rock - X = 1
+B - paper - Y = 2
+C - scissor - Z = 3
+-----------------
+0 for lost round
+3 for tied round
+6 for won round
+"""
+
 def main():
-    
+    with open('input.txt') as inputFile:
+        score = 0
+        for line in inputFile:
+            play1, play2 = line.split()
+
+            if play1 == 'A': # player 1 played rock
+                if play2 == 'Y': # player 2 played paper and won
+                    score += 8
+                elif play2 == 'X': # player 2 played rock and tied. 
+                    score += 4
+                else: # player 2 played scissors and lost
+                    score += 3
+
+            if play1 == 'B': # player 1 player paper
+                if play2 == 'Z': # player 2 plays scissor and won.
+                    score += 9
+                if play2 == 'Y': # player 2 plays paper and tied.
+                    score += 5
+                else:
+                    score += 1
+
+            if play1 == 'C': # player 1 player scissor 
+                if play2 == 'X':
+                    score += 7
+                if play2 == 'Z':
+                    score += 6
+                else:
+                    score += 2
+
+        print(score)
+
 
 
 
