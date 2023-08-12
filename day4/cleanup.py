7d848ffcf8f41c1225e36b89f77c78f64ef868a4
ahamad2 <abdu07@proton.me>
2023-08-11 21:34:22 -0700

    started day4 and added gitignore file

---
+++ day4/cleanup.py
@@ -0,0 +1,11 @@
+
+
+def main():
+    with open('input.txt') as inputFile:
+        numberContained = 0 # count the number of assignments where it is contained by another in the pair
+        for line in inputFile:
+            
+
+   
+if __name__=="__main__":
+    main()
