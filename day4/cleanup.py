f81c13a473ca35880576a07b2b65f3e52a5d4b02
ahamad2 <abdu07@proton.me>
2023-08-15 22:36:05 -0700

    changed structure to accomedate for part2 of day4

--- day4/cleanup.py
+++ day4/cleanup.py
@@ -1,13 +1,16 @@
 
-
-def main():
+def findPairDuplicates():
     with open('/home/vanishing/git/advent2022/day4/input.txt') as inputFile:
         numberContained = 0 # count the number of assignments where it is contained by another in the pair
         for line in inputFile:
             assignment1, assignment2 = line.split(',')
             min1, max1 = assignment1.split('-')
             min2, max2 = assignment2.split('-')
-
+            min1 = int(min1)
+            min2 = int(min2)
+            max1 = int(max1)
+            max2 = int(max2)
+            
             # first, check if the 1st assignment is contained in the second. 
             if min1 >= min2 and max1 <= max2:
                 numberContained += 1
@@ -18,6 +21,14 @@ def main():
                 numberContained += 1
             
         print(numberContained)
+
+
+def findPairOverlaps():
+    return
+
+def main():
+    # findDuplicates()
+    findPairOverlaps()
 
    
 if __name__ == "__main__":
